b2ef845672447052aded5f8cb77170351fc2c9c6
dolcetriade <vcelestialragev@gmail.com>
2017-10-28 00:34:40 -0700

    zap: Change zap mechanics to require some semblence of aim.
    Previously, zap did all of its damage as soon as you hit a target
    and the animation was just for show. Now, you continue doing damage
    as long as you aim at the target. You do not have to maintain aim
    constantly, just ensure you aim at the target once every interval
    (currently, the interval is 500ms). You will stop zapping if you
    get far enough away from the primary target. Zap will do incrementally
    more damage the longer you maintain your aim.

--- src/sgame/sg_main.cpp
+++ src/sgame/sg_main.cpp
@@ -2346,6 +2346,12 @@ void G_RunFrame( int levelTime )
 				}
 				else if ( i < MAX_CLIENTS )
 				{
+
+					// Do CBSE style thinking for clients too.
+					if ( auto* thinkingComponent = ent->entity->Get<ThinkingComponent>() )
+					{
+						thinkingComponent->Think();
+					}
 					G_RunClient( ent );
 					continue;
 				}
@@ -2400,7 +2406,6 @@ void G_RunFrame( int levelTime )
 	G_CalculateAvgPlayers();
 	G_SpawnClients( TEAM_ALIENS );
 	G_SpawnClients( TEAM_HUMANS );
-	G_UpdateZaps( msec );
 	Beacon::Frame( );
 
 	// Run Lua timers, if any...
