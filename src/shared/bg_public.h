843729bced837173dd7d63dc023df63f07f3e4e1
Thomas Debesse <dev@illwieckz.net>
2024-05-08 07:39:24 +0200

    cgame: parse modelRotation for player models, fix modelScale

--- src/shared/bg_public.h
+++ src/shared/bg_public.h
@@ -1324,6 +1324,7 @@ struct classModelConfig_t
 {
 	char   modelName[ MAX_QPATH ];
 	float  modelScale;
+	vec3_t modelRotation;
 	char   skinName[ MAX_QPATH ];
 	float  shadowScale;
 	char   hudName[ MAX_QPATH ];
