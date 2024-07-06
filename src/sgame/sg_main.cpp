737bbb46dd4c7416ddddb2658342f1770ea9ec08
DolceTriade <vcelestialragev@gmail.com>
2024-07-06 00:01:20 -0700

    gameplay: Add g_freeFundMax to give additional free funds
    This additional amount will be scaled with momentum.

--- src/sgame/sg_main.cpp
+++ src/sgame/sg_main.cpp
@@ -197,6 +197,7 @@ Cvar::Cvar<float> g_alienOffCreepRegenHalfLife("g_alienOffCreepRegenHalfLife", "
 
 Cvar::Cvar<int> g_teamImbalanceWarnings("g_teamImbalanceWarnings", "send 'Teams are imbalanced' messages every x seconds if >0", Cvar::NONE, 0);
 Cvar::Cvar<int> g_freeFundPeriod("g_freeFundPeriod", "every x seconds, players get funds for nothing", Cvar::NONE, DEFAULT_FREEKILL_PERIOD);
+Cvar::Cvar<int> g_freeFundMax("g_freeFundMax", "Maximum extra money to give scaled with momentum in addition to the 200. (Aliens get num/200)", Cvar::NONE, 800);
 // int instead of bool for now to avoid changing the serverinfo format
 Cvar::Range<Cvar::Cvar<int>> g_unlagged("g_unlagged", "whether latency compensation is enabled", Cvar::SERVERINFO, 1, 0, 1);
 Cvar::Cvar<std::string> g_disabledVoteCalls(
