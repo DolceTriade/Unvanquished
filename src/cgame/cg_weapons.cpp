4ea4918f5adda446201acc7ff432af203ec53721
DolceTriade <vcelestialragev@gmail.com>
2026-04-07 20:29:13 -0700

    mdriver: Cleanup old partical system before creating new

--- src/cgame/cg_weapons.cpp
+++ src/cgame/cg_weapons.cpp
@@ -2656,6 +2656,13 @@ void CG_HandleWeaponHitEntity( entityState_t *es, vec3_t origin )
 		if ( weapon == WP_MASS_DRIVER )
 		{
 			centity_t *attacker = ( attackerNum == cg.predictedPlayerState.clientNum ) ? &cg.predictedPlayerEntity : &cg_entities[ attackerNum ];
+
+			if ( CG_IsTrailSystemValid( &attacker->muzzleTS ) )
+			{
+				CG_DestroyTrailSystem( attacker->muzzleTS );
+				attacker->muzzleTS = nullptr;
+			}
+
 			attacker->muzzleTS = CG_SpawnNewTrailSystem( cgs.media.mdriverTS );
 			if ( !attacker->muzzleTS ) return;
 			attacker->muzzleTSDeathTime = cg.time + cg_teslaTrailTime.Get();
@@ -2714,6 +2721,13 @@ void CG_HandleWeaponHitWall( entityState_t *es, vec3_t origin )
 		if ( weapon == WP_MASS_DRIVER )
 		{
 			centity_t *attacker = ( attackerNum == cg.predictedPlayerState.clientNum ) ? &cg.predictedPlayerEntity : &cg_entities[ attackerNum ];
+
+			if ( CG_IsTrailSystemValid( &attacker->muzzleTS ) )
+			{
+				CG_DestroyTrailSystem( attacker->muzzleTS );
+				attacker->muzzleTS = nullptr;
+			}
+
 			attacker->muzzleTS = CG_SpawnNewTrailSystem( cgs.media.mdriverTS );
 			if ( !attacker->muzzleTS ) return;
 			attacker->muzzleTSDeathTime = cg.time + cg_teslaTrailTime.Get();
