7716abd6b5af126cb4d0f97735004e4c80cc8bcc
DolceTriade <vcelestialragev@gmail.com>
2026-04-11 22:36:04 -0700

    nix: Add initial flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix flake for building Unvanquished client, server, and tty-client targets";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        import ./nix {
+          inherit self system nixpkgs flake-utils;
+        }
+    )
+    // {
+      nixosModules = {
+        unvanquished = import ./nix/module.nix {
+          inherit self;
+        };
+        default = self.nixosModules.unvanquished;
+      };
+    };
+}
---
+++ nix/engine.nix
@@ -0,0 +1,116 @@
+{
+  self,
+  pkgs,
+}: let
+  lib = pkgs.lib;
+  common = import ./common.nix {
+    inherit pkgs;
+  };
+  naclRuntime = import ./nacl-runtime.nix {
+    inherit pkgs;
+  };
+
+  version =
+    if self ? shortRev
+    then self.shortRev
+    else "dirty";
+
+  src = lib.cleanSourceWith {
+    src = ../.;
+    filter = path: type: let
+      baseName = builtins.baseNameOf path;
+      relPath = lib.removePrefix (toString ../. + "/") (toString path);
+    in
+      !(baseName
+        == ".git"
+        || baseName == "build"
+        || baseName == "CMakeFiles"
+        || baseName == "CMakeCache.txt"
+        || lib.hasPrefix "result" baseName
+        || relPath == "flake.lock");
+  };
+
+  mkDaemonPackage = {
+    pname,
+    executable,
+    buildClient ? false,
+    buildServer ? false,
+    buildTtyClient ? false,
+  }:
+    pkgs.stdenv.mkDerivation {
+      inherit pname version src;
+
+      nativeBuildInputs = common.commonNativeBuildInputs;
+      buildInputs = common.commonBuildInputs;
+      passthru = {
+        inherit (naclRuntime) package supported;
+      };
+
+      cmakeFlags = [
+        "-G"
+        "Ninja"
+        "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+        "-DBUILD_CLIENT=${
+          if buildClient
+          then "ON"
+          else "OFF"
+        }"
+        "-DBUILD_SERVER=${
+          if buildServer
+          then "ON"
+          else "OFF"
+        }"
+        "-DBUILD_TTY_CLIENT=${
+          if buildTtyClient
+          then "ON"
+          else "OFF"
+        }"
+        "-DBUILD_CGAME=OFF"
+        "-DBUILD_SGAME=OFF"
+        "-DBUILD_GAME_NACL=OFF"
+        "-DUSE_BREAKPAD=OFF"
+        "-DUSE_CPP23=OFF"
+        "-DUSE_EXTERNAL_DEPS=OFF"
+        "-DUSE_EXTERNAL_DEPS_LIBS=OFF"
+        "-DUSE_PRECOMPILED_HEADER=ON"
+      ];
+
+      installPhase = ''
+        runHook preInstall
+        mkdir -p "$out/bin"
+        cp "${executable}" "$out/bin/${executable}"
+        ${naclRuntime.installSnippet}
+        runHook postInstall
+      '';
+
+      meta = with lib; {
+        description = "Unvanquished ${pname} target";
+        license = licenses.bsd2;
+        mainProgram = executable;
+        platforms = platforms.linux;
+      };
+    };
+
+  client = mkDaemonPackage {
+    pname = "unvanquished-client";
+    executable = "daemon";
+    buildClient = true;
+  };
+
+  server = mkDaemonPackage {
+    pname = "unvanquished-server";
+    executable = "daemonded";
+    buildServer = true;
+  };
+
+  ttyClient = mkDaemonPackage {
+    pname = "unvanquished-tty-client";
+    executable = "daemon-tty";
+    buildTtyClient = true;
+  };
+in {
+  inherit client server;
+
+  tty-client = ttyClient;
+  default = client;
+}
