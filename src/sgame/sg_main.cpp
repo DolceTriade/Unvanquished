1beb526c187bbe5b30590c8b338bf43dafe58657
DolceTriade <vcelestialragev@gmail.com>
2026-05-24 02:36:27 -0700

    overload: more balance pricing

--- src/sgame/sg_main.cpp
+++ src/sgame/sg_main.cpp
@@ -204,10 +204,10 @@ Cvar::Cvar<float> g_shove("g_shove", "force multiplier when pushing players", Cv
 Cvar::Cvar<bool> g_antiSpawnBlock("g_antiSpawnBlock", "push away players who block their spawns", Cvar::NONE, false);
 Cvar::Cvar<float> g_overloadCostPerPlayer("g_overloadCostPerPlayer",
 	"additional overload cost multiplier per extra player seen on a team; overload costs only go upward during a match",
-	Cvar::NONE, 0.55f);
+	Cvar::NONE, 0.20f);
 Cvar::Cvar<float> g_overloadUnlockCostSlope("g_overloadUnlockCostSlope",
 	"multiplier applied to authored unlock values when deriving overload unlock costs",
-	Cvar::NONE, 17.213f);
+	Cvar::NONE, 8.0f);
 Cvar::Cvar<float> g_overloadUnlockCostOffset("g_overloadUnlockCostOffset",
 	"flat amount added after scaling authored unlock values when deriving overload unlock costs",
 	Cvar::NONE, 0.0f);
